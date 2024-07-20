08d3f25ee2c6e63edf7e25b41a8e9e0ac1e75593
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-07-20 18:34:54 +0000

    update to ssh.tf and output.tf

--- output.tf
+++ output.tf
@@ -9,3 +9,13 @@ output "worker_node1_public_ip" {
 output "worker_node2_public_ip" {
   value = aws_instance.worker_node2.public_ip
 }
+
+output "ssh_private_key" {
+  value = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
+}
+
+output "ssh_public_key" {
+  value = tls_private_key.ssh_key.public_key_openssh
+}
+
