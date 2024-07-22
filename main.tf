7fd3bd46418343449b604b378918449feb11cdec
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-07-22 14:25:04 +0000

    updates to main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "tls_private_key" "example" {
+resource "tls_private_key" "k8s_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "ssh_key" {
-  content  = tls_private_key.example.private_key_pem
+resource "local_file" "ssh_private_key" {
+  content  = tls_private_key.k8s_key.private_key_pem
   filename = "${path.module}/id_rsa"
 }
 
-resource "local_file" "ssh_key_pub" {
-  content  = tls_private_key.example.public_key_pem
+resource "local_file" "ssh_public_key" {
+  content  = tls_private_key.k8s_key.public_key_pem
   filename = "${path.module}/id_rsa.pub"
 }
 
-resource "aws_key_pair" "generated_key" {
+resource "aws_key_pair" "k8s_key_pair" {
   key_name   = var.ssh_key_name
-  public_key = tls_private_key.example.public_key_openssh
+  public_key = tls_private_key.k8s_key.public_key_openssh
 }
 
 resource "aws_security_group" "k8s_sg" {
@@ -45,7 +45,7 @@ resource "aws_instance" "k8s_master" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   security_groups        = [aws_security_group.k8s_sg.name]
-  key_name               = aws_key_pair.generated_key.key_name
+  key_name               = aws_key_pair.k8s_key_pair.key_name
 
   root_block_device {
     volume_size           = 30
@@ -62,7 +62,7 @@ resource "aws_instance" "k8s_worker" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   security_groups        = [aws_security_group.k8s_sg.name]
-  key_name               = aws_key_pair.generated_key.key_name
+  key_name               = aws_key_pair.k8s_key_pair.key_name
 
   root_block_device {
     volume_size           = 30
