fe06edeee6e0f80ad917b94fc27ce7d53aacd8ad
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-08-02 14:52:18 +0000

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,36 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket2024"
-    key            = "terraform.tfstate"
+    key            = "my-my-terraform-state-bucket2024/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
-  }
-}
-
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-terraform-state-bucket"
-}
-
-resource "aws_s3_bucket_object" "directory" {
-  bucket = aws_s3_bucket.my_bucket.idd
-  key    = "terraform-state-folder/"
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks"
-  billing_mode   = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  tags = {
-    Name = "terraform-locks"
   }
 }
