ed08481baee6074258defdee8d4a0d579155e802
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-08-02 14:32:20 +0000

    updates

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket2024"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
