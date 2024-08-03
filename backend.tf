a253407df26b5a6e2d54bb5fec2352edc31b6e5f
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-08-03 13:28:14 +0000

    update s3 buscket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket2024"
+    bucket         = "my-terraform-state-bucket20124"
     key            = "my-my-terraform-state-bucket2024/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
