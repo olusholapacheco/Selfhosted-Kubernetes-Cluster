b84c63a83054c04bbb59be5ecc8fb227fd45d75b
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-08-03 13:31:28 +0000

    region update

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket20124"
     key            = "my-my-terraform-state-bucket2024/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform-locks"
   }
 }
